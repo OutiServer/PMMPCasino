5bfd0c9329837cf0e8a2ea1b0bd24b66b6dafb7b
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-07-24 10:03:09 +0900

    1x3スロットの当たり処理と、共通当たり処理の追加

--- src/games/SlotGame.php
+++ src/games/SlotGame.php
@@ -5,19 +5,23 @@ declare(strict_types=1);
 namespace outiserver\casino\games;
 
 use InvalidArgumentException;
-use outiserver\economycore\Database\Economy\EconomyData;
-use outiserver\economycore\EconomyCore;
+use outiserver\casino\database\slotconfig\SlotConfigData;
+use outiserver\casino\particles\DragonBreathParticle;
+use outiserver\casino\particles\ExplosionEmitterParticle;
 use outiserver\casino\CasinoMain;
 use outiserver\casino\database\slot\SlotData;
 use Ken_Cir\LibFormAPI\FormContents\CustomForm\ContentToggle;
 use Ken_Cir\LibFormAPI\Forms\CustomForm;
 use pocketmine\player\Player;
 use pocketmine\scheduler\ClosureTask;
+use pocketmine\utils\Random;
 use pocketmine\utils\TextFormat;
 
 class SlotGame extends BaseGame
 {
     private SlotData $slotData;
+
+    private SlotConfigData $slotConfigData;
 
     private int $runCount;
 
@@ -34,6 +38,7 @@ class SlotGame extends BaseGame
         parent::__construct($player, $plugin);
 
         $this->slotData = $slotData;
+        $this->slotConfigData = $this->plugin->getSlotConfigDataManager()->get($this->slotData->getParentId());
         $this->runCount = 0;
         $this->result = [];
         $this->slotX = 0;
@@ -52,6 +57,7 @@ class SlotGame extends BaseGame
                     return;
                 }
 
+                $this->economyData->removeMoney($this->slotData->getBet());
                 $this->payLines = [0];
                 $this->runSlot();
                 break;
@@ -146,7 +152,8 @@ class SlotGame extends BaseGame
                 if ($i < 1) $slotTitle .= "§f";
                 else $slotTitle .= "\n§f";
 
-                $rand = rand(0, 9);
+                #$rand = rand(0, 9);
+                $rand = 6;
                 for ($y = 0; $y < $this->slotY; $y++) {
                     if (!isset($this->result[$i][$y]) and $y < ($this->runCount - 1)) $this->result[$i][$y] = $rand;
 
@@ -173,7 +180,72 @@ class SlotGame extends BaseGame
     {
         parent::complete();
 
-        $this->plugin->playSoundPlayer($this->player, "random.levelup");
-        $this->player->sendMessage("処理終了");
+        $winLines = 0;
+        $jp = false;
+        // 1x3
+        if ($this->slotX === 1 and $this->slotY === 3) {
+            if ($this->result[0][0] === $this->result[0][1] and $this->result[0][0] === $this->result[0][2]) {
+                if ($this->result[0][0] === 7) $jp = true;
+                else $winLines++;
+            }
+        }
+
+        // JP以外の当たりのみ
+        if ($winLines > 0 and !$jp) {
+            $this->player->sendMessage("[Casino] [{$this->slotConfigData->getName()}]" . TextFormat::GOLD . "{$winLines}ライン当たり！ " . ($winLines * ($this->slotData->getBet() * $this->plugin->getConfig()->get("reduction_rate", 10))) . "円獲得！");
+        }
+        // JPのみのあたり
+        elseif ($winLines < 1 and $jp) {
+            $this->player->sendMessage("[Casino] [{$this->slotConfigData->getName()}]" . TextFormat::GOLD . "ジャックポット！ " . $this->slotConfigData->getJp() . "円獲得！");
+        }
+        // JPとJP以外両方
+        elseif ($winLines > 0 and $jp) {
+            $this->player->sendMessage("[Casino] [{$this->slotConfigData->getName()}]" . TextFormat::GOLD . "ジャックポットと{$winLines}ライン当たり！ " . ($this->slotConfigData->getJp() + (($this->slotData->getBet() * $this->plugin->getConfig()->get("reduction_rate", 10)) * $winLines)) . "円獲得！");
+
+        }
+
+        // 当たり共通
+        if ($jp or $winLines > 0) {
+            $this->plugin->playSoundPlayer($this->player, "random.levelup");
+        }
+
+        // JP共通処理
+        if ($jp) {
+            $this->plugin->getServer()->broadcastMessage("[Casino] [{$this->slotConfigData->getName()}]" . TextFormat::GREEN . "{$this->player->getName()}さんがJPを当て、" . ($this->slotConfigData->getJp()) . "円獲得しました、おめでとうございます！");
+            $this->plugin->playSoundPlayer($this->player, "raid.horn");
+            $this->economyData->addMoney($this->slotConfigData->getJp());
+
+            $random = new Random((int) (microtime(true) * 1000) + mt_rand());
+            $pos = $this->player->getPosition();
+            for($i = 0; $i < 100; ++$i){
+                $pos = $pos->add(
+                    $random->nextSignedFloat() * 5,
+                    $random->nextSignedFloat() * 5,
+                    $random->nextSignedFloat() * 5
+                );
+                $this->player->getWorld()->addParticle($pos, new DragonBreathParticle());
+                $this->player->getWorld()->addParticle($pos, new ExplosionEmitterParticle());
+            }
+
+            $this->slotConfigData->setLatestJP($this->slotConfigData->getJp());
+            $this->slotConfigData->setLatestJPPlayerXuid($this->player->getXuid());
+            $this->slotConfigData->setJp($this->plugin->getConfig()->get("default_slot_jp", 10000));
+            $this->plugin->getServer()->broadcastMessage("[Casino] [{$this->slotConfigData->getName()}] JPが{$this->plugin->getConfig()->get("default_slot_jp", 10000)}円にリセットされました");
+        }
+
+        // 普通のあたり
+        if ($winLines > 0) {
+            $getMoney = $winLines * ($this->slotData->getBet() * $this->plugin->getConfig()->get("reduction_rate", 10));
+            $this->economyData->addMoney($getMoney);
+        }
+
+        // You Lose
+        if (!$jp) {
+            $this->slotConfigData->addJp($this->slotData->getBet() * (count($this->payLines) - $winLines));
+
+            if ($winLines < 1) {
+                $this->player->sendMessage("[Casino] [{$this->slotConfigData->getName()}]" . TextFormat::YELLOW . " ハズレ");
+            }
+        }
     }
 }
